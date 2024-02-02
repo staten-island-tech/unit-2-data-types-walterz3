46e5aa98014332137902fa7befa6accdf50fae27
walterz3 <walterz3@nycstudents.net>
2024-02-02 12:26:54 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,27 @@
+""" name = "Walter"
+def greet(person):
+    print(person)
+greet(name)
+#1 and "1" are not the same 
+ """
+
+
+
+""" tenure = False
+def is_tenured(status):
+    if(status == True):
+        print("they have tenure")
+    else: 
+        print("they are not tenured") 
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)  """
+
+
+x = "this is a thing"
+y= x.split()
+z = y[0]
+print(y)
+print(z)
