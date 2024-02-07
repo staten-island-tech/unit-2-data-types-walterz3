4d51de65153bbaae391b9a0a2d771b3ada51598e
walterz3 <walterz3@nycstudents.net>
2024-02-07 12:20:19 -0500

    e

--- gcf.py
+++ gcf.py
@@ -1,9 +1,14 @@
+
 y = int(input("What is your number? "))
 
+
 def factors(x):
-  print("The factors of",x,"are")
+  print("The gcf of",x,"are")
   for i in range (1, x+1):
     if x%i == 0:
       print(i)
 
+
+
 factors(y)
+
