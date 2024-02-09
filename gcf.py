0f0d37740af704f58cecaa24b328fdace9169a82
walterz3 <walterz3@nycstudents.net>
2024-02-09 12:29:13 -0500

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -2,7 +2,7 @@
 import math
 z = int(input("What is your number? "))
 y = int(input("What is your 2nd number? "))
-def factors(x, y):
+""" def factors(x, y):
   print("The factors of",x and y,"are")
   for i in range (1, x+1 ):
     if x%i == 0:
@@ -11,9 +11,9 @@ def factor(r):
   print("The factors of",r,"are")
   for i in range (1, r+1):
     if r%i == 0:
-      print(i)
+      print(i) """
 
-print("the gcd is", math.gcd(z,y))
+print("the gcf is", math.gcd(z,y))
 
 
 
