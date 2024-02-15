b274a65e02aa86da978b9a93a33613bedf4ec511
walterz3 <walterz3@nycstudents.net>
2024-02-14 21:56:45 -0500

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -1,18 +1,18 @@
-def print_factors(x):
-    factors_list = []
-    for i in range(1, x + 1):
-       if x % i == 0:
-           factors_list.append(i)
-    return factors_list
-num1 = int(input("What is your first number: "))
-factors_num1 = print_factors(num1)
-num2 = int(input("What is your second number: "))
-factors_num2 = print_factors(num2)
-gcf = 1
-for num in factors_num1:
-    if num in factors_num2 and gcf < num:
-        gcf = num
-print("The greatest common factor of ", num1 , ' and', num2, "are: ", gcf)
+number1 = int(input("First Number: "))
+number2 = int(input("Second Number: "))
 
+def factors(num):
+  factorlist = []
+  for i in range(1, num+1):
+    if num%i==0:
+      factorlist.append(i)
+  return factorlist
 
+factors1 = factors(number1)
+factors2 = factors(number2)
+
+for i in factors1[::-1]:
+  if i in factors2:
+    print(i)
+    break
 
